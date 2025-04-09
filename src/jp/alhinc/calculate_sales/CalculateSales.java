870250b4698c4b66db6520883c504b25170fe514
maki_akiyasu <maki.akiyasu@alhinc.jp>
2025-04-09 14:49:28 +0900

    売上ファイルの集計機能の実装

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -1,10 +1,14 @@
 package jp.alhinc.calculate_sales;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
@@ -37,6 +41,71 @@ public class CalculateSales {
 		}
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		File[] files = new File("C:\\Users\\trainee1343\\Desktop\\売り上げ集計課題").listFiles();
+
+		//先にファイルの情報を格納する List(ArrayList) を宣言
+		List<File> rcdFiles = new ArrayList<>();
+
+		for(int i = 0; i < files.length ; i++) {
+			//files[i].getName()　files[i]の名前を取得
+
+			if((files[i].getName()).matches("^[0-9]{8}+.+rcd$")) {
+				//"[0-9]{8}+.+rcd$"(0～9の8桁の数字+.+rcd)の名前になっているファイルのみ、List(ArrayList)に追加
+				rcdFiles.add(files[i]);
+
+			}
+
+		}
+
+		for(int i = 0; i < rcdFiles.size(); i++) {
+			BufferedReader br = null;
+
+			try {
+				FileReader fr;
+
+				fr = new FileReader(rcdFiles.get(i));
+
+				br = new BufferedReader(fr);
+
+				List<String> list = new ArrayList<>();
+
+				String line;
+
+				while((line = br.readLine()) != null) {
+					//読んだものをリストに入れる
+					list.add(line);
+				}
+
+				//Qここ時点での　リスト　って中身がどうなってるっけ
+				//Q次何をしたいんだっけ
+
+				long fileSale = Long.parseLong(list.get(1));
+
+				Long saleAmount = (branchSales.get(list.get(0))) + (fileSale);
+
+				branchSales.put(list.get(0), saleAmount);
+
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				// e.printStackTrace();
+				System.out.println(UNKNOWN_ERROR);
+				return;
+
+			}finally {
+				// ファイルを開いている場合
+				if(br != null) {
+					try {
+						// ファイルを閉じる
+						br.close();
+					} catch(IOException e) {
+						System.out.println(UNKNOWN_ERROR);
+						return;
+					}
+				}
+			}
+			//branchSalesに値が正常に入ったか確認
+			System.out.println(branchSales);
+		}
 
 
 
@@ -72,13 +141,12 @@ public class CalculateSales {
 				//行を","で区切る→items[0]には,までの支店コード、items[1]には,より後ろの支店名が格納
 				String[] items = line.split(",");
 
-
+				//items[0]:支店コード　items[1]:支店名　0L:Long型の売上金額の初期値0
 				branchNames.put(items[0],items[1]);
 				branchSales.put(items[0],0L);
 
 				System.out.println(line);
-				System.out.println(branchNames.get(items[0]));
-				System.out.println(branchSales.get(items[0]));
+
 			}
 
 		} catch(IOException e) {
@@ -110,6 +178,34 @@ public class CalculateSales {
 	 */
 	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
 		// ※ここに書き込み処理を作成してください。(処理内容3-1)
+		File writefiles = new File(path,FILE_NAME_BRANCH_OUT);
+		BufferedWriter bw = null;
+
+
+			try {
+				bw = new BufferedWriter(new FileWriter(writefiles));
+
+				for(String key:branchNames.keySet()) {
+					bw.write(key + "," + branchNames.get(key) + "," + branchSales.get(key));
+					bw.newLine();
+				}
+
+			}catch(IOException e) {
+				System.out.println(UNKNOWN_ERROR);
+				return false;
+
+			}finally {
+				if(bw != null) {
+					try {
+						// ファイルを閉じる
+						bw.close();
+					} catch(IOException e) {
+						System.out.println(UNKNOWN_ERROR);
+						return false;
+					}
+				}
+
+			}
 
 		return true;
 	}
